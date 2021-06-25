ec76fd70e9d14962b657fd09fb341caa6764929b
danh1999yb <danhcon9933@gmail.com>
2021-06-25 21:15:29 +0700

    tai len cho vui

--- SQL/Assignment 1/Extra Assignment 1_2.sql
+++ SQL/Assignment 1/Extra Assignment 1_2.sql
@@ -1,43 +1,45 @@
 
 -- ex1
 DROP DATABASE IF EXISTS  Fresher_Training_Management;
-CREATE DATABASE  Fresher_Training_Management;
+CREATE DATABASE  IF NOT EXISTS  Fresher_Training_Management;
 USE Fresher_Training_Management;
 CREATE TABLE Trainee (
     TraineeID INT AUTO_INCREMENT PRIMARY KEY,
-    Full_Name VARCHAR(50),
-    Birth_Date VARCHAR(50),
+    Full_Name VARCHAR(50) NOT NULL,
+    Birth_Date DATE NOT NULL,
     Gender ENUM('male', 'female', 'unknown'),
-    ET_IQ VARCHAR(20),
-    ET_Gmath VARCHAR(20),
-    ET_English VARCHAR(50),
-    Training_Class VARCHAR(50),
-    Evaluation_Notes VARCHAR(50),
-    VTI_Account VARCHAR(50) NOT NULL UNIQUE
+    ET_IQ TINYINT UNSIGNED  ,
+    ET_Gmath TINYINT UNSIGNED ,
+    ET_English SMALLINT UNSIGNED,
+    Training_Class VARCHAR(50) ,
+    Evaluation_Notes VARCHAR(50)
+    
 );
 
+ALTER TABLE Trainee 
+ADD COLUMN VTI_Account VARCHAR(50) NOT NULL UNIQUE;
 
 -- ex2
 DROP DATABASE IF EXISTS  ex2;
-CREATE DATABASE  ex2;
+CREATE DATABASE IF NOT EXISTS  ex2;
 
 CREATE TABLE bang (
-    ID TINYINT  UNSIGNED AUTO_INCREMENT,
-    `Name` VARCHAR(20),
-    `Code` CHARACTER(5),
-    ModifiedDate DATETIME
+    ID TINYINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    `Name` VARCHAR(20) NOT NULL,
+    `Code` CHARACTER(5) NOT NULL,
+    ModifiedDate DATETIME 
 );
 
 -- ex3
 DROP DATABASE IF EXISTS  ex3;
-CREATE DATABASE  ex3;
+CREATE DATABASE IF NOT EXISTS  ex3;
 
 CREATE TABLE bang3 (
-    ID TINYINT UNSIGNED PRIMARY KEY   AUTO_INCREMENT ,
-    `NAME` VARCHAR(20),
+    ID TINYINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    `NAME` VARCHAR(20) NOT NULL,
     BirthDate DATE,
-    Gender ENUM('0', '1', 'NULL'),
-    IsDeletedFlag ENUM('0', '1')
+    Gender ENUM('0', '1', 'NULL') NOT NULL,
+    IsDeletedFlag ENUM('0', '1') NOT NULL
 );
 
 
